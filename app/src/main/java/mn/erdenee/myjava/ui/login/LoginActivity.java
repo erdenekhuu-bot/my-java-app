a723eabbab334a6c84f4ce5c1c1a02609f4a1095
Эрдэнэхүү Энхээ <erdenekhuu.e@gmobile.mn>
2025-06-25 17:20:56 +0800

    map

--- app/src/main/java/mn/erdenee/myjava/ui/login/LoginActivity.java
+++ app/src/main/java/mn/erdenee/myjava/ui/login/LoginActivity.java
@@ -1,10 +1,14 @@
 package mn.erdenee.myjava.ui.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import mn.erdenee.myjava.databinding.ActivityLoginBinding;
+import mn.erdenee.myjava.ui.map.MapsActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private ActivityLoginBinding binding;
 
     @Override
@@ -12,6 +16,13 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.loginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 
     @Override
