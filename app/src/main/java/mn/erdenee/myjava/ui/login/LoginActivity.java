b81dc11846f245d0127996a5b5d3228612b00948
Эрдэнэхүү Энхээ <erdenekhuu.e@gmobile.mn>
2025-06-25 17:04:47 +0800

    .

--- app/src/main/java/mn/erdenee/myjava/ui/login/LoginActivity.java
+++ app/src/main/java/mn/erdenee/myjava/ui/login/LoginActivity.java
@@ -2,15 +2,17 @@ package mn.erdenee.myjava.ui.login;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import mn.erdenee.myjava.databinding.ActivityLoginBinding;
 import mn.erdenee.myjava.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private ActivityLoginBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
