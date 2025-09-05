520f4f8838495e5b7e59642fb9e05eefe8333a5b
Эрдэнэхүү Энхээ <erdenekhuu.e@gmobile.mn>
2025-09-05 17:38:05 +0800

    .

--- app/src/main/java/mn/erdenee/myjava/api/ApiService.java
+++ app/src/main/java/mn/erdenee/myjava/api/ApiService.java
@@ -2,6 +2,9 @@ package mn.erdenee.myjava.api;
 
 import retrofit2.Call;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
+import retrofit2.http.Url;
+import retrofit2.http.GET;
 
 public interface ApiService {
 
