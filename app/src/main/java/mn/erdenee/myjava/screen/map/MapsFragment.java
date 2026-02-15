3233e639d124172b881f5952ded8026df3d679b3
erdenekhuu.e <erdenekhuu.e@gmobile.mn>
2026-02-15 17:53:51 +0800

    .

--- app/src/main/java/mn/erdenee/myjava/screen/map/MapsFragment.java
+++ app/src/main/java/mn/erdenee/myjava/screen/map/MapsFragment.java
@@ -1,27 +1,23 @@
 package mn.erdenee.myjava.screen.map;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import mn.erdenee.myjava.R;
@@ -33,10 +29,6 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, View.O
     private FusedLocationProviderClient fusedLocationProviderClient;
     private GoogleMap googleMap;
     private Location currentLocation;
-
-    private final int FINE_PERMISSION_CODE=1;
-
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
@@ -82,8 +74,10 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, View.O
 
     @Override
     public void onMapClick(LatLng latLng) {
+        BitmapDescriptor bitmapDescriptor =
+                BitmapDescriptorFactory.fromResource(R.drawable.custommarker);
         this.googleMap.clear();
-        this.googleMap.addMarker(new MarkerOptions().position(latLng).title("Сонгосон байршил"));
+        this.googleMap.addMarker(new MarkerOptions().position(latLng).title("Сонгосон байршил").icon(bitmapDescriptor));
         this.googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
         Log.d("TAG", "Сонгосон цэг: " + latLng.latitude + ", " + latLng.longitude);
     }
